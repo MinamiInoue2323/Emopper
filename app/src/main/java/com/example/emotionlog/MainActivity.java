296bd33b0df8a4faa8a68440171beaa43f7c9d67
MinamiInoue2323 <inoue@ics.keio.ac.jp>
2021-03-01 15:44:13 +0900

    initial commit

---
+++ app/src/main/java/com/example/emotionlog/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.emotionlog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.SayBuilder;
+import com.aldebaran.qi.sdk.design.activity.RobotActivity;
+import com.aldebaran.qi.sdk.object.conversation.Say;
+
+public class MainActivity extends RobotActivity implements RobotLifecycleCallbacks {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        QiSDK.register(this, this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        QiSDK.unregister(this, this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+        Say say = SayBuilder.with(qiContext)
+                .withText("お帰りなさい！今日は一日，どんな気持ちでした？")
+
+
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+
+    }
+}
